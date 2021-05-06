63ff82665ae4d1d9fc452748d6f3b78282e9f91a
Darryl Cauldwell <darrylcauldwell@users.noreply.github.com>
2021-05-06 13:18:54 +0100

    Update handler.ps1

--- handler.ps1
+++ handler.ps1
@@ -24,7 +24,8 @@ $headers = @{
    "Accept"  = "application/json"
    }
 
-## Bypass certificate issues
+## Bypass certificate issues -  Does not work on dotNet Core function uses -SkipCertificateCheck
+<#
 add-type @"
    using System.Net;
    using System.Security.Cryptography.X509Certificates;
@@ -37,6 +38,7 @@ add-type @"
    }
 "@
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+#>
 
 ## Acquire bearer token
 $uri = "https://" + $vropsFqdn + "/suite-api/api/auth/token/acquire"
@@ -49,7 +51,7 @@ $basicAuthBody = @{
 $basicAuthBodyJson = $basicAuthBody | ConvertTo-Json -Depth 5
 
 Write-Host "Acquiring bearer token ..."
-$bearer = Invoke-WebRequest -Uri $uri -Method POST -Headers $headers -Body $basicAuthBodyJson | ConvertFrom-Json
+$bearer = Invoke-WebRequest -Uri $uri -Method POST -Headers $headers -Body $basicAuthBodyJson -SkipCertificateCheck | ConvertFrom-Json
 Write-Host "Bearer token is" $bearer.token
 
 ## Form authorized headers payload
@@ -62,13 +64,12 @@ $authedHeaders = @{
 ## Get host ResourceID
 $uri = "https://" + $vropsFqdn + "/suite-api/api/adapterkinds/VMWARE/resourcekinds/HostSystem/resources?name=" + $esxiHost
 Write-Host "Acquiring host ResourceID ..."
-$resource = Invoke-WebRequest -Uri $uri -Method GET -Headers $authedHeaders
+$resource = Invoke-WebRequest -Uri $uri -Method GET -Headers $authedHeaders -SkipCertificateCheck
 $resourceJson = $resource.Content | ConvertFrom-Json
 Write-Host "ResourceID of host is " $resourceJson.resourceList[0].identifier
 
 ## Mark host as maintenance mode
-
 $uri = "https://" + $vropsFqdn + "/suite-api/api/resources/" + $resourceJson.resourceList[0].identifier + "/maintained"
 Write-Host "Marking host as vROps maintenance mode ..."
-Invoke-WebRequest -Uri $uri -Method PUT -Headers $authedHeaders
+Invoke-WebRequest -Uri $uri -Method PUT -Headers $authedHeaders -SkipCertificateCheck
 }
